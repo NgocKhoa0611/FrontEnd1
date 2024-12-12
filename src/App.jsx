8cd3002a639d6b35c308034de4b920c170665598
thangleth <leethang090@gmail.com>
2024-12-13 04:31:15 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminRoutes from "./routes/AdminRoutes";
 import UserRoutes from "./routes/UserRoutes";
-import PrivateRoute from "./routes/PrivateRoute"; // Import PrivateRoute
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/admin/*" element={<PrivateRoute element={<AdminRoutes />} />} />
-        <Route path="/*" element={<UserRoutes />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/admin/*" element={<AdminRoutes />} />
+      <Route path="/*" element={<UserRoutes />} />
+    </Routes>
   );
 }
 
